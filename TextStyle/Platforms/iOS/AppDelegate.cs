3eac25eb08c294f8bc3fa52e7dfed826a344948c
Kornel Pakulski <kornel.pakulski@teb.edu.pl>
2024-05-21 12:16:20 +0200

    Dodaj pliki projektów.

---
+++ TextStyle/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TextStyle
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
